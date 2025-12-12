456302da3d64df93a8d91253800b26d4dc10a6e1
Maxim <maxzyuzkov@gmail.com>
2025-12-12 15:55:59 +0300

    Task 3 complete

--- collision.py
+++ collision.py
@@ -1,3 +1,19 @@
 def isCorrectRect(rect):
     (x1,y1), (x2,y2) = rect
     return x1 < x2 and y1 < y2
+
+def isCollisionRect(rect1,rect2):
+    if not isCorrectRect (rect1):
+        raise Exception("1 прямоугольник неправильный.")
+    if not isCorrectRect(rect2):
+        raise Exception("2 прямоугольник неверный.")
+
+    x1,y1=rect1[0]
+    x2,y2 = rect1[1]
+    x3,y3=rect2[0]
+    x4,y4 = rect2[1]
+    if x2 < x3 or x4 < x1:
+        return False
+    if y2 < y3 or y4 < y1:
+        return False
+    return True
