292baf5104288ea0836208eecfbe5fa561c86001
Maxim <maxzyuzkov@gmail.com>
2025-12-12 15:59:43 +0300

    Task 5 complete

--- collision.py
+++ collision.py
@@ -38,3 +38,37 @@ def intersectionAreaRect(rect1,rect2):
         return width*height
     else:
         return 0
+
+def intersectionAreaMultiRect(rects):
+    if not rects:
+        return 0
+    for i,rect in enumerate(rects,start = 1):
+        if not isCorrectRect (rect):
+            raise Exception("{i} прямоугольник неправильный.")
+    (x1,y1),(x2,y2) = rects[0]
+    x_right = max(x1,x2)
+    x_left = min(x1,x2)
+    y_bottom = min(y1,y2)
+    y_high = max(y1,y2)
+    for rect in rects[1:]:
+        (x1,y1),(x2,y2) = rect
+        x_right1 = max(x1,x2)
+        x_left1 = min(x1,x2)
+        y_bottom1 = min(y1,y2)
+        y_high1 = max(y1,y2)
+
+        right = min(x_right,x_right1)
+        left = max(x_left,x_left1)
+        bottom = max(y_bottom,y_bottom1)
+        high = min(y_high,y_high1)
+   
+        if right <= left or high <= bottom:
+            return 0
+        x_right = right
+        x_left = left
+        y_bottom = bottom
+        y_high = high
+
+        width = x_right - x_left
+        height = y_high - y_bottom
+        return width*height
